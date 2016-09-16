9796bcba1ccc39e08c4603a1d7db8ea839a19996
LaurentFer <laurent.ferrer@me.com>
2016-09-16 08:23:32 +0200

    Ajout Ligne de commentaire
    Pour test

--- TestW3S/src/com/programme/MonConvertisseurCF.java
+++ TestW3S/src/com/programme/MonConvertisseurCF.java
@@ -13,22 +13,23 @@ public class MonConvertisseurCF {
 
 	/**
 	 * @param args
+	 * Test ajout ligne
 	 */
 	public static void main(String[] args) {
 		
 		//j'instancie un nouvel objet de type TempConvert
 		TempConvert monImageObjet = new TempConvert() ;
 		
-		//Je fais appel a une méthode de cet objet pour avoir une image de mon interface disposant des méthodes de conversions
+		//Je fais appel a une mÃ©thode de cet objet pour avoir une image de mon interface disposant des mÃ©thodes de conversions
 		TempConvertSoap convertisseur = monImageObjet.getTempConvertSoap() ;
 		
-		//Je déclare et initialise 2 variables pour faire des test, une en C, une en F
+		//Je dÃ©clare et initialise 2 variables pour faire des test, une en C, une en F
 		String temperatureC = "40" ;
 		String temperatureF = "104" ;
 		
-		//Je fais appel aux méthodes de conversion et j'affiche le résultat
-		System.out.println(temperatureC +"°C = " +convertisseur.celsiusToFahrenheit(temperatureC) +" °F" );
-		System.out.println(temperatureF +"°F = " +convertisseur.fahrenheitToCelsius(temperatureF) +" °C" );
+		//Je fais appel aux mÃ©thodes de conversion et j'affiche le rÃ©sultat
+		System.out.println(temperatureC +"Â°C = " +convertisseur.celsiusToFahrenheit(temperatureC) +" Â°F" );
+		System.out.println(temperatureF +"Â°F = " +convertisseur.fahrenheitToCelsius(temperatureF) +" Â°C" );
 		
 
 	}
